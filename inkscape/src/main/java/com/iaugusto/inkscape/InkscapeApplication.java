6d103dc15d8346b4fca33067c0936deea92a97ff
Igor Augusto <igoraugustobz@gmail.com>
2025-03-23 15:34:35 -0300

    :tada: Commit Inicial

---
+++ inkscape/src/main/java/com/iaugusto/inkscape/InkscapeApplication.java
@@ -0,0 +1,13 @@
+package com.iaugusto.inkscape;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InkscapeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InkscapeApplication.class, args);
+	}
+
+}
