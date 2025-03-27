0c3cb566c8371e6bd90e09b8dd57c02cada610ad
Igor Augusto <igoraugustobz@gmail.com>
2025-03-27 00:12:38 -0300

    :recycle: refactor: modificar para comportar uma lista de Address

--- inkscape/src/main/java/com/iaugusto/inkscape/model/dto/UserDTO.java
+++ inkscape/src/main/java/com/iaugusto/inkscape/model/dto/UserDTO.java
@@ -1,6 +1,28 @@
 package com.iaugusto.inkscape.model.dto;
 
 import com.iaugusto.inkscape.model.entities.Address;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import org.hibernate.validator.constraints.br.CPF;
 
-public record UserDTO(String name, String email, String password, Address address, String cellphone, String cpf) {
-}
+import java.util.List;
+
+public record UserDTO(
+        @NotNull
+        String name,
+
+        @Email
+        @NotNull
+        String email,
+
+        @NotNull
+        String password,
+
+        List<Address> address,
+
+        String cellphone,
+
+        @CPF(message = "CPF inválido. O formato é XXX.XXX.XXX-XX")
+        @NotNull
+        String cpf
+) {}
