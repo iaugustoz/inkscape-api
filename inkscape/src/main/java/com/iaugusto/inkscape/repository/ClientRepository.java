e70d9a2fee7fe324e77bfa137ff32c5ffff77fdf
Igor Augusto <igoraugustobz@gmail.com>
2025-03-31 21:41:06 -0300

    :recycle: refactor: renomear arquivo

--- inkscape/src/main/java/com/iaugusto/inkscape/repository/ClientRepository.java
+++ inkscape/src/main/java/com/iaugusto/inkscape/repository/ClientRepository.java
@@ -1,15 +1,15 @@
 package com.iaugusto.inkscape.repository;
 
-import com.iaugusto.inkscape.model.entities.User;
+import com.iaugusto.inkscape.model.entities.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface ClientRepository extends JpaRepository<Client, Long> {
 
     boolean existsByEmail(String email);
 
-    Optional<User> findByEmail(String email);
+    Optional<Client> findByEmail(String email);
 }
